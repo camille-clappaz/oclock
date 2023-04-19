f3fbea363e57adb57759e6b8fa1bcf030e665200
camille-clappaz <camille.clappaz@laplateforme.io>
2023-04-19 15:36:44 +0200

    chrono OK!

--- chronometre.php
+++ chronometre.php
@@ -1,39 +1,93 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <div class="chronometre">
-        <form action="">
-            <button id="marche" type="submit" name="marche">Marche/Arrêt</button>
-            <button type="submit" name="tour">Tour</button>
-            <button type="reset" name="reset">Reset</button>
-        </form>
-
+        <span id="chrono">00:00:00</span><br>
+        <button id="marcheArret">Démarrer</button>
+        <button id="tour" type="submit" name="tour">Tour</button>
+        <button id="reset">Réinitialiser</button>
     </div>
-    <div class="tour">
+    <div id="tours">
 
     </div>
     <script>
-        let chrono = document.querySelector(".chronometre");
-        let p= document.createElement("p");
-        chrono.prepend(p);
-        
-        let heure = 0;
-        let mili = 0;
-        let sec = 0;
-        // affichage du compteur à 0
-        p.innerHTML ="0" + heure + ":" + "0" + sec + ":" + "0" + mili;
-        let marche= document.getElementById("marche");
-        marche.addEventListener("click", ()=>{
-          
-        })
-      
-    </script>
-    
+    let chrono = null;
+    let startTime = null;
+    let pausedTime = null;
+    let tours = [];
+
+    const playPauseBtn = document.getElementById("marcheArret");
+    const tourBtn = document.getElementById("tour");
+    const resetBtn = document.getElementById("reset");
+    const chronoElement = document.getElementById("chrono");
+    const toursList = document.getElementById("tours");
+
+    function updateTimer() {
+      const elapsedTime = Date.now() - startTime;
+      const hours = Math.floor(elapsedTime / 3600000);
+      const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+      const seconds = Math.floor((elapsedTime % 60000) / 1000);
+      const formattedTime = `${padNumber(hours)}:${padNumber(minutes)}:${padNumber(seconds)}`;
+      chronoElement.textContent = formattedTime;
+    }
+
+    function startPauseTimer() {
+      if (!startTime) {
+        startTime = Date.now();
+        playPauseBtn.textContent = "Pause";
+        timer = setInterval(updateTimer, 1000);
+      } else if (!pausedTime) {
+        pausedTime = Date.now();
+        playPauseBtn.textContent = "Reprendre";
+        clearInterval(timer);
+      } else {
+        const pauseDuration = Date.now() - pausedTime;
+        startTime += pauseDuration;
+        pausedTime = null;
+        playPauseBtn.textContent = "Pause";
+        timer = setInterval(updateTimer, 1000);
+      }
+    }
+
+    function tour() {
+      const elapsedTime = Date.now() - startTime;
+      const lapTime = elapsedTime - (tours.length > 0 ? tours[tours.length - 1] : 0);
+      tours.push(elapsedTime);
+      const lapNumber = tours.length;
+      const formattedTime = `${padNumber(Math.floor(lapTime / 3600000))}:${padNumber(Math.floor((lapTime % 3600000) / 60000))}:${padNumber(Math.floor((lapTime % 60000) / 1000))}`;
+      const lapItem = document.createElement("li");
+      lapItem.textContent = `Tour ${lapNumber}: ${formattedTime}`;
+      toursList.appendChild(lapItem);
+    }
+
+    function resetTimer() {
+      clearInterval(timer);
+      startTime = null;
+      pausedTime = null;
+      tours = [];
+      chronoElement.textContent = "00:00:00";
+      while (toursList.firstChild) {
+        toursList.removeChild(toursList.firstChild);
+      }
+      playPauseBtn.textContent = "Démarrer";
+    }
+
+    function padNumber(number) {
+      return number.toString().padStart(2, "0");
+    }
+
+    playPauseBtn.addEventListener("click", startPauseTimer);
+    tourBtn.addEventListener("click", tour);
+    resetBtn.addEventListener("click", resetTimer);
+  </script>
 </body>
+
 </html>
--- minuteur.php
+++ minuteur.php
@@ -8,72 +8,58 @@
     <title>Document</title>
 </head>
 <div class="minuteur">
-<form action="">
-    <input id="heure" type="text">
-    <label for="heure">heure</label>
-    <input id="minute" type="text">
-    <label for="minute">minute</label>
-    <input id="seconde" type="text">
-    <label for="seconde">seconde</label><br>
-    <button id="go" type="submit" name="go">Go</button>
-</form>
+        <input id="heure" type="text">
+        <label for="heure">heure</label>
+        <input id="minute" type="text">
+        <label for="minute">minute</label>
+        <input id="seconde" type="text">
+        <label for="seconde">seconde</label><br>
+        <button id="go" type="submit" name="go">Go</button>
 </div>
 
 <body>
     <script>
-      
-     
-     let go=document.getElementById("go");
-    //  let min=document.getElementById("minute");
-    //  console.log(go);
-    const departMinutes = 1 ;
-    // let temps = departMinutes * 60;
-    
-    const departHeures = 1;
-// let temps2 = departHeures * 60 ;
+        let btn = document.querySelector("button");
+        btn.addEventListener("click", () => {
+                const departMinutes = document.getElementById("minute").value;
+                const departHeures = document.getElementById("heure").value;
+                const departSec = document.getElementById("seconde").value;
+                let time = departHeures * 3600 + departMinutes * 60 + departSec;
+                // console.log(time)
 
-const departSec = 1;
-// let temps3 = departSec;
+            if (time === 0) { console.log("woop");
+    console.log("Le minuteur est terminé!");}
+            else {
+                // e.preventDefault();
+                let minuteur = document.querySelector(".minuteur");
+                let timerElement = document.createElement("p");
+                minuteur.prepend(timerElement);
 
-let time = departHeures*3600 + departMinutes * 60 + departSec ;
-console.log(time)
+                setInterval(() => {
+                    //
+                    let heures = parseInt(time / 3600, 10)
+                    console.log(time)
 
-        
-       
+                    let minutes = parseInt(time % 3600 / 60, 10)
+                    let secondes = parseInt(time % 60, 10)
+                    console.log(heures, minutes, secondes)
+                    //
+                    //
+                    heures = heures < 1 ? "0" + heures : heures
+                    minutes = minutes < 10 ? "0" + minutes : minutes
+                    console.log(minutes)
+                    secondes = secondes < 10 ? "0" + secondes : secondes
+                    console.log(secondes)
+                    // 
 
-        let minuteur=document.querySelector(".minuteur");
-        let timerElement = document.createElement("p");
-        minuteur.prepend(timerElement);
+                    timerElement.innerText = `${heures}:${minutes}:${secondes}`;
+                    time = time <= 0 ? 0 : time - 1;
+                    console.log(time);
+                }, 1000);
+                
 
-        setInterval(() => {
-            //
-            let heures = parseInt(time / 3600, 10)
-            console.log(time)
-        
-            let minutes = parseInt(time % 3600 / 60, 10)
-            let secondes = parseInt(time % 60, 10)
-            console.log(heures,minutes,secondes)
-//
-            //
-            heures = heures < 1 ? "0" + heures : heures
-            minutes = minutes < 10 ? "0" + minutes : minutes
-            console.log(minutes)
-            secondes = secondes < 10 ? "0" + secondes : secondes
-            console.log(secondes)
-            // 
-
-            timerElement.innerText = `${heures}:${minutes}:${secondes}`;
-            time = time <= 0 ? 0 : time - 1;
-            console.log(time);
-            // temps = temps <= 0 ? 0 : temps - 1
-            
-            // temps2 = temps2 <= 0 ? 0 : temps2 - 1
-            // temps3 = temps3 <= 0 ? 0 : temps3 - 1
-        }, 1000)
-
-
-
-    
+            } 
+        })
     </script>
 
 </body>
